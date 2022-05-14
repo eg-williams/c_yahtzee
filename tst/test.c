e4c76515762be270df06b662eebf7af5eff33ee6
Emyr Williams <egwilliams2002@googlemail.com>
2022-05-14 20:39:41 +0100

    Adding Unity test framework to code. Should be making use of git submodule

---
+++ tst/test.c
@@ -0,0 +1,20 @@
+#include <unity/unity.h>
+#include "../src/yahtzee.h"
+
+void setUp(void) {
+
+}
+
+void tearDown(void) {
+
+}
+
+void test_all_ones(void) {
+
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_all_ones);
+    return UNITY_END();
+}
